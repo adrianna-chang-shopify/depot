609b779c8fba2e1589da0eca1e3fd60919f67dde
Adrianna Chang <adrianna.chang@shopify.com>
2017-03-23 13:31:57 -0400

    Add controller to handle customer-centric interactions, change styling of pages, write some more functional tests, implement fragment caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #Define root path and add store_index_url to ensure that existing code/tests continue to work
+  root 'store#index', as: 'store_index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
